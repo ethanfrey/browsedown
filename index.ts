a2b024f8632754a3b4101a9a4741aecbf4cd1247
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-05-10 21:11:16 +0200

    Start project

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { AbstractLevelDOWN } from "abstract-leveldown";
+
+type O = any;
+type GO = any;
+type PO = any;
+type DO = any;
+type K = any;
+type V = any;
+
+class BrowseDown extends AbstractLevelDOWN {
+  _open(opts: O, callback: (err?: any) => void): void {}
+
+  _close(callback: (err?: any) => void): void {}
+
+  _get(key: K, options: GO, callback: (err: any, value: V) => any): void {}
+
+  _put(key: K, value: V, options: PO, callback: (err: any) => any): void {}
+
+  _del(key: K, options: DO, callback: (err: any) => any): void {}
+}
