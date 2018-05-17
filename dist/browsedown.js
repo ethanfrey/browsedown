96225412dc8a9c4fd8c3deeef5a4e36033078e8a
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-05-17 22:05:36 +0200

    Try to build importable library with webpack (umd)

--- dist/browsedown.js
+++ dist/browsedown.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -230,3 +240,4 @@ eval("/* WEBPACK VAR INJECTION */(function(process) {\nObject.defineProperty(exp
 /***/ })
 
 /******/ });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -17,30 +17,12 @@ const config = {
   },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
-  }
-};
-
-// module build is normal build to be imported in another
-// project
-const moduleOut = {
+  },
   output: {
     filename: 'browsedown.js',
     path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd'
   }
 };
 
-// // we need the library build, so we can test in the browser
-// // with karma tests
-// const libaryOut = {
-//   output: {
-//     filename: 'library.js',
-//     path: path.resolve(__dirname, 'dist'),
-//     libraryTarget: 'var',
-//     library: 'BrowseDown'
-//   }
-// };
-
-module.exports = [
-  Object.assign({}, config, moduleOut)
-  // ,Object.assign({}, config, libaryOut)
-];
+module.exports = config;
