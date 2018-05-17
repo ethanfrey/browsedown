e01d4075c90a381987aba3ba74eb194512fb1864
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-05-17 19:55:36 +0200

    Clean up tape tests

--- test/simple_spec.ts
+++ test/simple_spec.ts
@@ -1,62 +1,59 @@
 /* jshint esversion: 6 */
-import test from 'tape';
+import {AbstractLevelDOWN} from 'abstract-leveldown';
+import {TestCase} from 'tape';
 
-import {BrowseDown, makeDB} from '../src';
+type Factory = (name: string) => AbstractLevelDOWN;
+type Test = (name: string, cb: TestCase) => void;
 
-test('always passes', t => {
-  t.equal(1+1, 2);
-  t.end();
-});
-
-// test('never passes', t => {
-//   t.equal(2, 3);
-//   t.end();
-// });
-
-test('create and delete databases', t => {
-  const store = makeDB('spec-store-simple', () => {
-    store.deleteDatabase(
-      () => { t.equal(1, 1); t.end() },  // just to show some success
-      (err: any) => { t.error(err); t.end(); }
-    );
-  });
-});
-
-test('should open and close db', t => {
-  const store = new BrowseDown('open-close-test');
-  // TODO: is there a way to avoid these callbacks?
-  store.open((err: any) => {
-    t.error(err);
-    store.close((err2: any) => {
-      t.error(err2);
-      t.end();
-    });
-  });
-});
-
-test('should put and get data', t => {
-  const store = new BrowseDown('put-get-test-2');
-  // TODO: is there a way to avoid these callbacks?
-  store.open((err: any) => {
-    t.error(err);
-    store.get('foo', (err2: any, noval?: any) => {
-      // not found error....
-      // t.assert(err, 'Should be NotFoundError: ' + noval);
-      // let's add it now
-      store.put('foo', 'bar', { raw: true }, (err3: any) => {
-        t.error(err3);
-        store.get('foo', (err4: any, val: any) => {
-          // not found error....
-          t.error(err4);
-          t.assert(val);
-          t.equals('bar', val.toString());                                           
-          store.close((err5: any) => {
-            t.error(err5);
-            t.end();
-          });
-        });                
+function openClose(test: Test, factory: Factory): void {
+  test('should open and close db', t => {
+    const store = factory('open-close-test');
+    // TODO: is there a way to avoid these callbacks?
+    store.open((err: any) => {
+      t.error(err);
+      store.close((err2: any) => {
+        t.error(err2);
+        t.end();
       });
     });
   });
-});
-  
+};
+
+function readWrite(test: Test, factory: Factory): void {
+  test('should put and get data', t => {
+    const store = factory('put-get-test-2');
+    // TODO: is there a way to avoid these callbacks?
+    store.open((err: any) => {
+      t.error(err);
+      store.get('foo', (err2: any, noval?: any) => {
+        // not found error....
+        t.assert(err2, 'Should be NotFoundError: ' + noval);
+        // let's add it now
+        store.put('foo', 'bar', { raw: true }, (err3: any) => {
+          t.error(err3);
+          store.get('foo', (err4: any, val: any) => {
+            // not found error....
+            t.error(err4);
+            t.assert(val);
+            t.equals('bar', val.toString());                                           
+            store.close((err5: any) => {
+              t.error(err5);
+              t.end();
+            });
+          });                
+        });
+      });
+    });
+  });
+};
+
+function all(test: Test, factory: Factory): void {
+  openClose(test, factory);
+  readWrite(test, factory);
+};
+
+module.exports = {
+  all,
+  openClose,
+  readWrite
+}
--- test/webpack.tests.js
+++ test/webpack.tests.js
@@ -1,4 +1,10 @@
 /* jshint esversion: 6 */
 import test from 'tape';
 
+import {BrowseDown} from '../src';
 import simple from './simple_spec';
+
+
+const factory = (name) => new BrowseDown(name);
+
+simple.all(test, factory);
